f7e21d43fc4c4dddb269abf3236e127e0809a518
Yusuke Furuta <furuta@hololab.co.jp>
2023-03-03 17:04:45 +0900

    update

--- packages/jp.co.hololab.positioningtools.nmea/Scripts/NmeaDeviceService.cs
+++ packages/jp.co.hololab.positioningtools.nmea/Scripts/NmeaDeviceService.cs
@@ -191,17 +191,4 @@ namespace HoloLab.PositioningTools.Nmea
             }
         }
     }
-
-    public class NmeaDeviceInfo
-    {
-        public Func<Task<NmeaDevice>> CreateMethod { get; }
-        public string DisplayName { get; }
-        public override string ToString() => DisplayName;
-
-        public NmeaDeviceInfo(string displayName, Func<Task<NmeaDevice>> createMethod)
-        {
-            DisplayName = displayName;
-            CreateMethod = createMethod;
-        }
-    }
 }
