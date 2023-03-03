73119fee97aee0cbdd309eac47734282992b9f23
tarukosu <4415085+tarukosu@users.noreply.github.com>
2023-03-03 16:28:38 +0900

    Add support for AR Core Geospatial API (#1)

---
+++ packages/jp.co.hololab.positioningtools.arcoreextensions/Samples~/GeospatialAPI/Scripts/CoordinateManagerBindingText.cs
@@ -0,0 +1,36 @@
+using HoloLab.PositioningTools.CoordinateSystem;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HoloLab.PositioningTools.ARCoreExtensions.Samples
+{
+    public class CoordinateManagerBindingText : MonoBehaviour
+    {
+        [SerializeField]
+        private Text text = null;
+
+        private void Start()
+        {
+            CoordinateManager.Instance.OnCoordinatesBound += OnCoordinatesBound;
+        }
+
+        private void OnCoordinatesBound(WorldBinding worldBinding)
+        {
+            var pose = worldBinding.ApplicationPose;
+            var geodeticPose = worldBinding.GeodeticPose;
+            var geodeticPosition = geodeticPose.GeodeticPosition;
+
+            var builder = new StringBuilder();
+            builder.AppendLine($"[UnityPose] Position: {pose.position}, EnuRotation: {pose.rotation.eulerAngles}");
+            builder.AppendLine($"[GeodeticPose]");
+            builder.AppendLine($"Latitude: {geodeticPosition.Latitude}");
+            builder.AppendLine($"Longitude: {geodeticPosition.Longitude}");
+            builder.AppendLine($"EllipsoidalHeight: {geodeticPosition.EllipsoidalHeight}, EnuRotation: {geodeticPose.EnuRotation.eulerAngles}");
+
+            text.text = builder.ToString();
+        }
+    }
+}
