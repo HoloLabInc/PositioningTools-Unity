89563fcb0e222dfe54835a0c82b2d297d7657475
Yusuke Furuta <furuta@hololab.co.jp>
2023-03-03 10:27:31 +0900

    update

---
+++ packages/jp.co.hololab.positioningtools.arcoreextensions/Scripts/ARCoreSpatialApiService.cs
@@ -0,0 +1,83 @@
+﻿using Google.XR.ARCoreExtensions;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine.XR.ARSubsystems;
+
+namespace HoloLab.PositioningTools.ARCoreExtensions
+{
+    public class ARCoreSpatialApiService : ICardinalDirectionService, IGeographicLocationService
+    {
+        private readonly AREarthManager earthManager;
+
+        private bool isRunning;
+
+        private const int loopIntervalMilliseconds = 100;
+
+        public event Action<CardinalDirection> OnDirectionUpdated;
+        public event Action<GeographicLocation> OnLocationUpdated;
+
+        public ARCoreSpatialApiService(AREarthManager arEarthManager)
+        {
+            earthManager = arEarthManager;
+        }
+
+        public async void StartService()
+        {
+            await StartServiceAsync();
+        }
+
+        public Task<(bool ok, Exception exception)> StartServiceAsync()
+        {
+#if UNITY_IOS
+            UnityEngine.Input.location.Start();
+#endif
+            if (!isRunning)
+            {
+                isRunning = true;
+                var getCompassLoop = new Task(() =>
+                {
+                    _ = GetCompassLoop();
+                });
+                getCompassLoop.RunSynchronously();
+            }
+            return Task.FromResult<(bool, Exception)>((true, null));
+        }
+
+        public async void StopService()
+        {
+            await StopServiceAsync();
+        }
+
+        public Task<(bool ok, Exception exception)> StopServiceAsync()
+        {
+#if UNITY_IOS
+            UnityEngine.Input.location.Stop();
+#endif
+            isRunning = false;
+            return Task.FromResult<(bool, Exception)>((true, null));
+        }
+
+        private async Task GetCompassLoop()
+        {
+            while (isRunning)
+            {
+                var earthTrackingState = earthManager.EarthTrackingState;
+                if (earthTrackingState == TrackingState.Tracking)
+                {
+                    var pose = earthManager.CameraGeospatialPose;
+
+                    var dateTimeOffset = DateTimeOffset.Now;
+                    var heading = (float)pose.Heading;
+                    var data = new CardinalDirection(heading, dateTimeOffset);
+                    OnDirectionUpdated?.Invoke(data);
+
+                    var geographicLocation = new GeographicLocation(pose.Latitude, pose.Longitude, pose.Altitude, dateTimeOffset);
+                    OnLocationUpdated?.Invoke(geographicLocation);
+                }
+
+                await Task.Delay(loopIntervalMilliseconds);
+            }
+        }
+    }
+}
---
+++ packages/jp.co.hololab.positioningtools.arcoreextensions/Scripts/ARCoreSpatialApiServiceComponent.cs
@@ -0,0 +1,52 @@
+﻿using Google.XR.ARCoreExtensions;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace HoloLab.PositioningTools.ARCoreExtensions
+{
+    public class ARCoreSpatialApiServiceComponent : MonoBehaviour, ICardinalDirectionService, IGeographicLocationService
+    {
+        [SerializeField]
+        private AREarthManager arEarthManager = null;
+
+        private ARCoreSpatialApiService arCoreSpatialApiService;
+
+        public event Action<CardinalDirection> OnDirectionUpdated;
+        public event Action<GeographicLocation> OnLocationUpdated;
+
+        private void Awake()
+        {
+            arCoreSpatialApiService = new ARCoreSpatialApiService(arEarthManager);
+            arCoreSpatialApiService.OnDirectionUpdated += x => OnDirectionUpdated?.Invoke(x);
+            arCoreSpatialApiService.OnLocationUpdated += x => OnLocationUpdated?.Invoke(x);
+        }
+
+        private void OnDestroy()
+        {
+            arCoreSpatialApiService.StopService();
+        }
+
+        public void StartService()
+        {
+            arCoreSpatialApiService.StartService();
+        }
+
+        public Task<(bool ok, Exception exception)> StartServiceAsync()
+        {
+            return arCoreSpatialApiService.StartServiceAsync();
+        }
+
+        public void StopService()
+        {
+            arCoreSpatialApiService.StopService();
+        }
+
+        public Task<(bool ok, Exception exception)> StopServiceAsync()
+        {
+            return arCoreSpatialApiService.StopServiceAsync();
+        }
+    }
+}
