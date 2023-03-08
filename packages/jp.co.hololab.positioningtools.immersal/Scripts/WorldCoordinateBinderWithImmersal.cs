46f49e5bc1b42974ed2792d1aed3017d8de5e05b
Yusuke Furuta <furuta@hololab.co.jp>
2023-03-08 13:00:04 +0900

    Add scripts

---
+++ packages/jp.co.hololab.positioningtools.immersal/Scripts/WorldCoordinateBinderWithImmersal.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using HoloLab.PositioningTools.CoordinateSystem;
+using HoloLab.PositioningTools.GeographicCoordinate;
+using Immersal.AR;
+using UnityEngine;
+using ImmersalCore = Immersal.Core;
+
+namespace HoloLab.PositioningTools.Immersal
+{
+    public class WorldCoordinateBinderWithImmersal : MonoBehaviour
+    {
+        private LocalizerBase arLocalizer;
+        private CoordinateManager coordinateManager;
+
+        private void Start()
+        {
+            coordinateManager = CoordinateManager.Instance;
+            arLocalizer = GetComponentInChildren<LocalizerBase>();
+            arLocalizer.OnPoseFound += OnPoseFound;
+        }
+
+        private void OnPoseFound(LocalizerPose localizerPose)
+        {
+            BindSpaceCoordinate(localizerPose);
+            BindWorldCoordinate(localizerPose);
+        }
+
+        private void BindSpaceCoordinate(LocalizerPose localizerPose)
+        {
+            var spaceType = SpaceOrigin.SpaceTypeImmersal;
+            var mapId = arLocalizer.lastLocalizedMapId.ToString();
+
+            if (ARSpace.mapIdToMap.TryGetValue(arLocalizer.lastLocalizedMapId, out var arMap))
+            {
+                var spaceBinding = new SpaceBinding(arMap.transform, spaceType, mapId);
+                coordinateManager.BindSpace(spaceBinding);
+            }
+        }
+
+        private void BindWorldCoordinate(LocalizerPose localizerPose)
+        {
+            var ecefPosition = UnityPositionToEcef(Vector3.zero, localizerPose);
+            var geodeticPosition = GeographicCoordinateConversion.EcefToGeodetic(ecefPosition);
+
+            var forwardEnu = UnityPositionToEnu(Vector3.forward, localizerPose, geodeticPosition);
+
+            // For the vertical direction, use the Y-axis of the Unity application
+            forwardEnu.y = 0;
+
+            var applicationPose = new Pose(Vector3.zero, Quaternion.identity);
+            var geodeticPose = new GeodeticPose(
+                geodeticPosition,
+                Quaternion.LookRotation(forwardEnu, Vector3.up)
+            );
+            var binding = new WorldBinding(applicationPose, geodeticPose);
+            coordinateManager.BindCoordinates(binding);
+        }
+
+        private static Vector3 UnityPositionToEnu(Vector3 position, LocalizerPose localizerPose, GeodeticPosition geodeticPosition)
+        {
+            var ecefPosition = UnityPositionToEcef(position, localizerPose);
+
+            var enuPosition = GeographicCoordinateConversion.EcefToEnu(
+                ecefPosition.X, ecefPosition.Y, ecefPosition.Z,
+                geodeticPosition.Latitude, geodeticPosition.Longitude, geodeticPosition.EllipsoidalHeight);
+
+            return enuPosition.ToUnityVector();
+        }
+
+        private static EcefPosition UnityPositionToEcef(Vector3 position, LocalizerPose localizerPose)
+        {
+            Vector3 positionInMap = localizerPose.matrix.MultiplyPoint(position);
+
+            var ecefArray = new double[3];
+            ImmersalCore.PosMapToEcef(ecefArray, ARHelper.SwitchHandedness(positionInMap), localizerPose.mapToEcef);
+            var ecefPosition = new EcefPosition(ecefArray[0], ecefArray[1], ecefArray[2]);
+            return ecefPosition;
+        }
+    }
+}
---
+++ packages/jp.co.hololab.positioningtools.immersal/UI/Scripts/ImmersalSignInUI.cs
@@ -0,0 +1,86 @@
+using Immersal;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HoloLab.PositioningTools.Immersal
+{
+    public class ImmersalSignInUI : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_InputField emailAddressInputField = null;
+
+        [SerializeField]
+        private TMP_InputField passwordInputField = null;
+
+        [SerializeField]
+        private TMP_InputField serverUrlInputField = null;
+
+        [SerializeField]
+        private Button signInButtton = null;
+
+        private ImmersalMapManager immersalMapManager;
+
+        private const string emailAddressInputFieldKey = "ImmersalSignInUI_emailAddressInputField";
+        private const string passwordInputFieldKey = "ImmersalSignInUI_passwordInputField";
+        private const string serverUrlInputFieldKey = "ImmersalSignInUI_serverUrlInputField";
+
+        private ImmersalSDK immersalSDK;
+
+        public void Initialize(ImmersalMapManager immersalMapManager)
+        {
+            this.immersalMapManager = immersalMapManager;
+        }
+
+        private void Start()
+        {
+            immersalSDK = ImmersalSDK.Instance;
+
+            LoadSettings();
+            signInButtton.onClick.AddListener(Login);
+
+            immersalMapManager.OnLogin += ImmersalMapManager_OnLogin;
+        }
+
+        private void LoadSettings()
+        {
+            var emailAddress = PlayerPrefs.GetString(emailAddressInputFieldKey);
+            emailAddressInputField.text = emailAddress;
+
+            var password = PlayerPrefs.GetString(passwordInputFieldKey);
+            passwordInputField.text = password;
+
+            var serverUrl = PlayerPrefs.GetString(serverUrlInputFieldKey, ImmersalSDK.DefaultServer);
+            serverUrlInputField.text = serverUrl;
+        }
+
+        private async void Login()
+        {
+            var emailAddress = emailAddressInputField.text.Trim();
+            var password = passwordInputField.text.Trim();
+            var serverUrl = serverUrlInputField.text.Trim();
+
+            PlayerPrefs.SetString(emailAddressInputFieldKey, emailAddress);
+            PlayerPrefs.SetString(passwordInputFieldKey, password);
+
+            if (serverUrl != ImmersalSDK.DefaultServer)
+            {
+                immersalSDK.localizationServer = serverUrl;
+                PlayerPrefs.SetString(serverUrlInputFieldKey, serverUrl);
+            }
+            PlayerPrefs.Save();
+
+            signInButtton.interactable = false;
+            await immersalMapManager.LoginAsync(emailAddress, password);
+            signInButtton.interactable = true;
+        }
+
+        private void ImmersalMapManager_OnLogin()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
