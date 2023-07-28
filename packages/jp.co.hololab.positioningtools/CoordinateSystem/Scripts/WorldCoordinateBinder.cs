065f0049837473fedcc0c5562ff2ed76a5d1e251
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2023-07-28 15:20:45 +0900

    update

--- packages/jp.co.hololab.positioningtools/CoordinateSystem/Scripts/WorldCoordinateBinder.cs
+++ packages/jp.co.hololab.positioningtools/CoordinateSystem/Scripts/WorldCoordinateBinder.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 namespace HoloLab.PositioningTools.CoordinateSystem
 {
     /// <summary>
-    /// A component for binding between Unity coordinates and geographic coordinates to debug in Unty Editor.
+    /// A component for binding between Unity coordinates and geographic coordinates.
     /// </summary>
-    public class WorldCoordinateBinderForEditor : MonoBehaviour
+    public class WorldCoordinateBinder : MonoBehaviour
     {
+        private enum RuntimeType
+        {
+            None = 0,
+            Editor = 1,
+            Player = 2,
+            EditorAndPlayer = 3,
+        }
+
         [SerializeField]
         [Tooltip("latitude/longitude/height")]
         private GeodeticPositionForInspector geodeticPosition;
@@ -15,11 +23,12 @@ namespace HoloLab.PositioningTools.CoordinateSystem
         [SerializeField]
         private float northHeading;
 
+        [SerializeField]
+        private RuntimeType runtimeType = RuntimeType.Editor;
+
         private void Start()
         {
-#if UNITY_EDITOR
             StartCoroutine(Bind());
-#endif
         }
 
         private IEnumerator Bind()
@@ -35,6 +44,29 @@ namespace HoloLab.PositioningTools.CoordinateSystem
 
             var binding = new WorldBinding(pose, gPose);
             spaceCoordinateManager.BindCoordinates(binding);
+        }
+
+        private bool IsBindingValid()
+        {
+#if UNITY_EDITOR
+            switch (runtimeType)
+            {
+                case RuntimeType.Editor:
+                case RuntimeType.EditorAndPlayer:
+                    return true;
+                default:
+                    return false;
+            }
+#else
+            switch (runtimeType)
+            {
+                case RuntimeType.Player:
+                case RuntimeType.EditorAndPlayer:
+                    return true;
+                default:
+                    return false;
+            }
+#endif
         }
     }
 }
