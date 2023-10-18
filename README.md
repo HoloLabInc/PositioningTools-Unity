ac0259bfa77974af747113c8d59cbf7a100ad635
tarukosu <4415085+tarukosu@users.noreply.github.com>
2023-10-19 08:54:16 +0900

    Add a upm package and a Unity project for QR tracking (#27)

--- README.md
+++ README.md
@@ -31,6 +31,23 @@ Open `Packages\manifest.json` and add the following lines in "dependencies".
 "jp.co.hololab.positioningtools.nmea": "https://github.com/HoloLabInc/PositioningTools-Unity.git?path=packages/jp.co.hololab.positioningtools.nmea",
 ```
 
+## Setup for QR marker tracking
+
+#### Requirements
+
+- ARFoundation
+- [ARFoundationQRTracking-Unity](https://github.com/HoloLabInc/ARFoundationQRTracking-Unity)
+
+#### Import
+
+Open `Packages\manifest.json` and add the following lines in "dependencies".
+
+```
+"jp.co.hololab.positioningtools": "https://github.com/HoloLabInc/PositioningTools-Unity.git?path=packages/jp.co.hololab.positioningtools",
+"jp.co.hololab.positioningtools.arfoundationmarker": "https://github.com/HoloLabInc/PositioningTools-Unity.git?path=packages/jp.co.hololab.positioningtools.arfoundationmarker",
+"jp.co.hololab.positioningtools.geographiccoordinate": "https://github.com/HoloLabInc/PositioningTools-Unity.git?path=packages/jp.co.hololab.positioningtools.geographiccoordinate",
+```
+
 ## Setup for Immersal
 
 #### Requirements
@@ -72,6 +89,7 @@ Open `Packages\manifest.json` and add the following lines in "dependencies".
 
 - [PositioningTools-ARCore-GeospatialAPI](./unity/PositioningTools-ARCore-GeospatialAPI)
 - [PositioningTools-NMEA-ARFoundation](./unity/PositioningTools-NMEA-ARFoundation)
+- [PositioningTools-Marker-ARFoundation](./unity/PositioningTools-Marker-ARFoundation)
 - [PositioningTools-Immersal-ARFoundation](./unity/PositioningTools-Immersal-ARFoundation)
 - [PositioningTools-VuforiaAreaTarget](./unity/PositioningTools-VuforiaAreaTarget)
 
---
+++ packages/jp.co.hololab.positioningtools.arfoundationmarker/QRTracking/Scripts/CoordinateBinderWithQRTracking.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using HoloLab.ARFoundationQRTracking;
+using HoloLab.PositioningTools.CoordinateSystem;
+using UnityEngine;
+
+namespace HoloLab.PositioningTools.Immersal
+{
+    public class CoordinateBinderWithQRTracking : MonoBehaviour
+    {
+        private CoordinateManager coordinateManager;
+        private ARFoundationQRTracker qrTracker;
+
+        private const string spaceType = SpaceOrigin.SpaceTypeMarker;
+
+        private void Start()
+        {
+            coordinateManager = CoordinateManager.Instance;
+
+            qrTracker = FindObjectOfType<ARFoundationQRTracker>();
+            qrTracker.OnTrackedQRImagesChanged += QRTracker_OnTrackedQRImagesChanged;
+        }
+
+        private void QRTracker_OnTrackedQRImagesChanged(ARTrackedQRImagesChangedEventArgs eventArgs)
+        {
+            foreach (var addedQR in eventArgs.Added)
+            {
+                var spaceBinding = ARTrackedQRImageToSpaceBinding(addedQR);
+                if (addedQR.TrackingReliable)
+                {
+                    coordinateManager.BindSpace(spaceBinding);
+                }
+            }
+
+            foreach (var updatedQR in eventArgs.Updated)
+            {
+                var spaceBinding = ARTrackedQRImageToSpaceBinding(updatedQR);
+                if (updatedQR.TrackingReliable)
+                {
+                    coordinateManager.BindSpace(spaceBinding);
+                }
+            }
+
+            foreach (var removedQR in eventArgs.Removed)
+            {
+                var spaceBinding = ARTrackedQRImageToSpaceBinding(removedQR);
+                coordinateManager.UnbindSpace(spaceBinding);
+            }
+        }
+
+        private static SpaceBinding ARTrackedQRImageToSpaceBinding(ARTrackedQRImage qr)
+        {
+            var qrTransform = qr.transform;
+            var pose = new Pose(qrTransform.position, qrTransform.rotation);
+            var spaceBinding = new SpaceBinding(pose, spaceType, qr.Text);
+            return spaceBinding;
+        }
+    }
+}
