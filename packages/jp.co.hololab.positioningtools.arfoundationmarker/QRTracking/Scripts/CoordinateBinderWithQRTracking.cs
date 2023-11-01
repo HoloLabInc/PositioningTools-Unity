a71794315ffeeec68212a7f3ca6d7bdc1e44dea9
Yusuke Furuta <furuta.yusuke.012@gmail.com>
2023-11-01 14:00:29 +0900

    update

--- packages/jp.co.hololab.positioningtools.arfoundationmarker/QRTracking/Scripts/CoordinateBinderWithQRTracking.cs
+++ packages/jp.co.hololab.positioningtools.arfoundationmarker/QRTracking/Scripts/CoordinateBinderWithQRTracking.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using HoloLab.ARFoundationQRTracking;
+using HoloLab.PositioningTools.CoordinateSerialization;
 using HoloLab.PositioningTools.CoordinateSystem;
 using UnityEngine;
 
@@ -10,6 +12,9 @@ namespace HoloLab.PositioningTools.ARFoundationMarker
     {
         private CoordinateManager coordinateManager;
         private ARFoundationQRTracker qrTracker;
+
+        private readonly Dictionary<ARTrackedQRImage, ICoordinateInfo> coordinateInfoDictionary = new Dictionary<ARTrackedQRImage, ICoordinateInfo>();
+        private readonly CoordinateSerializer coordinateSerializer = new CoordinateSerializer();
 
         private const string spaceType = SpaceOrigin.SpaceTypeMarker;
 
@@ -32,26 +37,54 @@ namespace HoloLab.PositioningTools.ARFoundationMarker
         {
             foreach (var addedQR in eventArgs.Added)
             {
-                var spaceBinding = ARTrackedQRImageToSpaceBinding(addedQR);
+                if (coordinateSerializer.TryDeserialize(addedQR.Text, out var coordinateInfo))
+                {
+                    coordinateInfoDictionary[addedQR] = coordinateInfo;
+                }
+
                 if (addedQR.TrackingReliable)
                 {
-                    coordinateManager.BindSpace(spaceBinding);
+                    BindSpaceCoordinates(addedQR);
+                    BindWorldCoordinates(addedQR);
                 }
             }
 
             foreach (var updatedQR in eventArgs.Updated)
             {
-                var spaceBinding = ARTrackedQRImageToSpaceBinding(updatedQR);
                 if (updatedQR.TrackingReliable)
                 {
-                    coordinateManager.BindSpace(spaceBinding);
+                    BindSpaceCoordinates(updatedQR);
+                    BindWorldCoordinates(updatedQR);
                 }
             }
 
             foreach (var removedQR in eventArgs.Removed)
             {
+                // Space binding
                 var spaceBinding = ARTrackedQRImageToSpaceBinding(removedQR);
                 coordinateManager.UnbindSpace(spaceBinding);
+
+                // World binding
+                coordinateInfoDictionary.Remove(removedQR);
+            }
+        }
+
+        private void BindSpaceCoordinates(ARTrackedQRImage qr)
+        {
+            var spaceBinding = ARTrackedQRImageToSpaceBinding(qr);
+            coordinateManager.BindSpace(spaceBinding);
+        }
+
+        private void BindWorldCoordinates(ARTrackedQRImage qr)
+        {
+            if (coordinateInfoDictionary.TryGetValue(qr, out var coordinateInfo) == false)
+            {
+                return;
+            }
+
+            if (TryConvertARTrackedQRImageToWorldBinding(qr, coordinateInfo, out var worldBinding))
+            {
+                coordinateManager.BindCoordinates(worldBinding);
             }
         }
 
@@ -61,6 +94,31 @@ namespace HoloLab.PositioningTools.ARFoundationMarker
             var pose = new Pose(qrTransform.position, qrTransform.rotation);
             var spaceBinding = new SpaceBinding(pose, spaceType, qr.Text);
             return spaceBinding;
+        }
+
+        private static bool TryConvertARTrackedQRImageToWorldBinding(ARTrackedQRImage qr, ICoordinateInfo coordinateInfo, out WorldBinding worldBinding)
+        {
+            switch (coordinateInfo)
+            {
+                case GeodeticPositionWithHeading geodeticPositionWithHeading:
+                    {
+                        var qrTransform = qr.transform;
+                        var position = qrTransform.position;
+
+                        // TODO
+                        // var rotation = Quaternion.identity;
+                        var rotation = qrTransform.rotation;
+                        var pose = new Pose(position, rotation);
+
+                        var geodeticRotation = Quaternion.AngleAxis(geodeticPositionWithHeading.Heading, Vector3.up);
+                        var geodeticPose = new GeodeticPose(geodeticPositionWithHeading.GeodeticPosition, geodeticRotation);
+                        worldBinding = new WorldBinding(pose, geodeticPose);
+                        return true;
+                    }
+            }
+
+            worldBinding = null;
+            return false;
         }
     }
 }
